1e5ad60cab5fdba52f42e9d8ff606db8ead97e31
Eungae-D <s4078942@naver.com>
2024-06-28 02:28:34 +0900

    Feat : kakao 로그인

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+const onKakaoLogin = () => {
+  window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
+};
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={onKakaoLogin}>Kakao Login</button>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  );
+}
+
+const fetchData = () => {
+  axios
+    .get("http://localhost:8080/", { withCredentials: true })
+    .then((res) => {
+      alert(JSON.stringify(res.data));
+    })
+    .catch((error) => alert(error));
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
